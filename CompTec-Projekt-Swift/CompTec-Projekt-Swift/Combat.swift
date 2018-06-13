b953b8fe3e01a92420c74f3de57ddeb2778f9fb1
Ulrik's beundrer <ostehandler@hotmail.dk>
2018-06-13 09:05:29 +0200

    Added combat.swift

---
+++ CompTec-Projekt-Swift/CompTec-Projekt-Swift/Combat.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class combat {
+    var hp = 0
+    var attackDamage = 0
+    var xpWhenKilled = 0
+    var combatLevel = 0
+    
+    init(hp: Int, attackDamage: Int, xpWhenKilled: Int, combatLevel: Int) {
+        self.hp = hp
+        self.attackDamage = attackDamage
+        self.xpWhenKilled = xpWhenKilled
+        self.combatLevel = combatLevel
+    }
+    
+    func attack() {
+        
+    }
+    func run() {
+        
+    }
+    func win() {
+        
+    }
+    func lose() {
+        
+    }
+    
+
+}
