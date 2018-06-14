bb57dca623fbde58ab1750f856ab21858c1b1b18
dog <ulri1122@gmail.com>
2018-06-14 11:00:40 +0200

    login virker

--- CompTec-Projekt-Swift/CompTec-Projekt-Swift/main.swift
+++ CompTec-Projekt-Swift/CompTec-Projekt-Swift/main.swift
@@ -1,11 +1,16 @@
 import Foundation
 
 
-Farm.pickFarm()
+//Farm.pickFarm()
 var login = Login()
-
 print("username")
 var username = readLine()!
 print("password")
 var password = readLine()!
 login.login(username: username, password: password)
+sleep(1)
+var playername = login.userpass
+print(playername)
+var dude = readLine()
+
+
