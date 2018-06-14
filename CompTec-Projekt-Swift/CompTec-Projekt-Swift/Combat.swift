f879e22e03bb70b648df5f8595b2eda798ecfe3e
Unknown <falken39033@gmail.com>
2018-06-14 13:31:11 +0200

    Fixed

--- CompTec-Projekt-Swift/CompTec-Projekt-Swift/Combat.swift
+++ CompTec-Projekt-Swift/CompTec-Projekt-Swift/Combat.swift
@@ -30,11 +30,8 @@ class Combat {
 
 // Function for starting combat. Used when a Farm place is picked.
     func startFight() {
-        Thread.sleep(forTimeInterval: 1)
         print("It is your turn! What are you going to do?")
-        Thread.sleep(forTimeInterval: 1)
         print("Write 0 for help")
-        Thread.sleep(forTimeInterval: 1)
         
         let fight = readLine()
         
@@ -43,9 +40,7 @@ class Combat {
         
         case "0"?:
             print("Write 1 to Attack")
-            Thread.sleep(forTimeInterval: 1)
             print("Write 2 to Run Escape")
-            Thread.sleep(forTimeInterval: 1)
             startFight()
             
         case "1"?:
@@ -57,7 +52,6 @@ class Combat {
             
         default:
             print("You cant just stand there and do nothing!")
-            Thread.sleep(forTimeInterval: 1)
             startFight()
         }
         
@@ -67,19 +61,19 @@ class Combat {
     func attack() {
         
         currentEnemy.currentHealth = currentEnemy.currentHealth - currentPlayer.attackPower
-        Thread.sleep(forTimeInterval: 1)
+        
         print("You attacked the \(currentEnemy.type), You dealt \(currentPlayer.attackPower) Damage!")
-        Thread.sleep(forTimeInterval: 1)
+        
         print("The \(currentEnemy.type) now has \(currentEnemy.currentHealth) HP left!")
-        Thread.sleep(forTimeInterval: 1)
+        
         
         
         if currentEnemy.currentHealth <= 0
         {
             print("You killed the \(currentEnemy)")
-            Thread.sleep(forTimeInterval: 1)
+            
             win()
-            Thread.sleep(forTimeInterval: 1)
+            
         }
         else if currentPlayer.currentHealth <= 0
         {
@@ -96,18 +90,18 @@ class Combat {
     // This function lets you escape a battle, in case your characters combatlevel is too low to handle the fighting.
     
     func run() {
-        Thread.sleep(forTimeInterval: 1)
+        
         print("You sucessfully ran away from the \(currentEnemy)")
-        Thread.sleep(forTimeInterval: 1)
+        
         lose()
     }
     
     // This function lets the Enemy strike back at you, whenever you attack it, effectively creating a turn based combat system.
     func enemyTurn() {
         currentPlayer.currentHealth = currentPlayer.currentHealth - currentEnemy.attackPower
-        Thread.sleep(forTimeInterval: 1)
+        
         print("The \(currentEnemy.type) dealt \(currentEnemy.attackPower) to you!")
-        Thread.sleep(forTimeInterval: 1)
+        
         
         if currentPlayer.currentHealth <= 0 {
             lose()
@@ -116,14 +110,16 @@ class Combat {
     
     // This function is called when the Enemy's HP drops to or below 0. You then win the fight, heal back up, and gain some xp, which when you have enough, will increase your combat level. It then calls the "pickFarm()" function, to let you grind up to beat those nasty Dragons!
     func win() {
-        Thread.sleep(forTimeInterval: 1)
+        
         print("You won the battle!")
-        Thread.sleep(forTimeInterval: 1)
+        
         currentPlayer.combatLevelxp = currentPlayer.combatLevelxp + 10
         currentPlayer.currentHealth = 100
         print("You rest, and heal back to full HP. Your HP is now \(currentPlayer.currentHealth)")
+        print("You gained 10 XP! Your current XP is \(currentPlayer.combatLevelxp)")
         if currentPlayer.combatLevelxp >= 100 {
             currentPlayer.combatLevel = currentPlayer.combatLevel + 1
+            print("You leveled up! Your combat level is now \(currentPlayer.combatLevel)")
         }
         farmPlaceholder.pickFarm()
     }
@@ -131,9 +127,9 @@ class Combat {
     // This function is called either when your Characters HP drops to or below 0, or when you run. It is designed in such a way, that you don't lose the game even if you run, only if you actually died.
     
     func lose() {
-        Thread.sleep(forTimeInterval: 1)
+        
         print("You lost the battle!")
-        Thread.sleep(forTimeInterval: 1)
+        
         if currentPlayer.currentHealth <= 0 {
             print("You died! Restarting game!")
             gameStart()
